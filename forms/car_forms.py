72fb65b94884ae74bba29c9faf716ee7e6f00fb5
rajkumar <rajkumardatascientist59@gmail.com>
2025-02-23 09:11:31 +0530

    Update car_forms.py

--- forms/car_forms.py
+++ forms/car_forms.py
@@ -6,10 +6,11 @@ from wtforms import StringField, IntegerField, FloatField, TextAreaField, Submit
 from wtforms.validators import DataRequired, Length, NumberRange, ValidationError, Optional
 from datetime import datetime
 
+
 class CarForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired(), Length(max=100)])
     description = TextAreaField("Description", validators=[DataRequired()])
-    price = IntegerField("Price", validators=[DataRequired(), NumberRange(min=1)])
+    price = IntegerField("Price (INR)", validators=[DataRequired(), NumberRange(min=1)])
     year = IntegerField(
         "Year", validators=[DataRequired(), NumberRange(min=2015, max=datetime.now().year)]
     )
@@ -25,23 +26,27 @@ class CarForm(FlaskForm):
         validators=[FileAllowed(["jpg", "jpeg", "png", "gif"])],  # Include GIF
         render_kw={"multiple": True},
     )  # Allow multiple files
-    registration_card = FileField(
-        "Registration Card",
-        validators=[FileAllowed(["jpg", "jpeg", "png"])],
+
+    kilometers = IntegerField("Kilometers Driven", validators=[Optional()])
+    no_of_owners = IntegerField(
+        "Number of Previous Owners", validators=[DataRequired(), NumberRange(min=1, max=10)]
     )
-    kilometers = IntegerField("Kilometers Driven", validators=[DataRequired()])  # Add this line
-    no_of_owners = IntegerField("Number of Previous Owners", validators=[DataRequired(), NumberRange(min=1, max=10)])  # Add this line - and max
-    state = SelectField("State", choices=[], validators=[DataRequired()])  # New field
-    district = SelectField("District", choices=[], validators=[DataRequired()])  # New field, ADD DataRequired
+    state = SelectField("State", choices=[], validators=[DataRequired()])
+    district = SelectField("District", choices=[], validators=[DataRequired()])
     body_type = StringField("Body Type", validators=[Optional()])
-    fuel_type = StringField("Fuel Type", validators=[Optional()])
+    fuel_type = SelectField(
+        "Fuel Type",
+        choices=[("Petrol", "Petrol"), ("Diesel", "Diesel"), ("CNG", "CNG"), ("Electric", "Electric"), ("Hybrid", "Hybrid")],
+        validators=[Optional()],
+    )
     engine_type = StringField("Engine Type", validators=[Optional()])
-    engine_capacity = FloatField("Engine Capacity", validators=[Optional()])
+    engine_capacity = FloatField("Engine Capacity (CC)", validators=[Optional()])
     exterior_color = StringField("Exterior Color", validators=[Optional()])
     interior_color = StringField("Interior Color", validators=[Optional()])
     vin = StringField("VIN", validators=[Optional(), Length(max=17)])
     license_plate = StringField("License Plate", validators=[Optional()])
     registration_expiry = StringField("Registration Expiry", validators=[Optional()])
+
 
     submit = SubmitField("Post Ad")
 
@@ -50,11 +55,12 @@ class CarForm(FlaskForm):
         # Populate state choices.  This needs to happen *after* the form is instantiated
         try:
             from locations import indian_states_districts
-            self.state.choices = [(state, state) for state in indian_states_districts.keys()] #State selection
+
+            self.state.choices = [(state, state) for state in indian_states_districts.keys()]  # State selection
             self.district.choices = []
         except ImportError:
-            self.state.choices = [('No Locations', 'No Locations')] # if can't find location file
-            self.district.choices = [('No Locations', 'No Locations')] # if can't find location file
+            self.state.choices = [("No Locations", "No Locations")]  # if can't find location file
+            self.district.choices = [("No Locations", "No Locations")]  # if can't find location file
 
     def validate_year(self, year):
         if year.data < 2015:
