b531a0756220dca2de029996c904e03d06c6fa41
rajkumar <rajkumardatascientist59@gmail.com>
2025-02-24 11:54:25 +0530

    Update car.py

--- models/car.py
+++ models/car.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship
 from flask import current_app
 from datetime import datetime, timezone
 
+
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -26,21 +27,23 @@ class Car(db.Model):
     license_plate = db.Column(db.String(20))
     registration_expiry = db.Column(db.Date)
     registration_number = db.Column(db.String(20), nullable=True)
-    state = db.Column(db.String(50))  #  New field
-    city = db.Column(db.String(50)) # New field
+    state = db.Column(db.String(50))  # New field
+    city = db.Column(db.String(50))  # New field
     engine_type = db.Column(db.String(50))
     engine_capacity = db.Column(db.Float)  # e.g., 2.0 for 2.0L engine
-    no_of_owners = db.Column(db.Integer) # Added no_of_owners
+    no_of_owners = db.Column(db.Integer)  # Added no_of_owners
     seller_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
     is_approved = db.Column(db.Boolean, default=False)
     is_featured = db.Column(db.Boolean, default=False)  # Add the is_featured column
-    date_posted = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc))
+    date_posted = db.Column(db.DateTime(timezone=True),
+                             default=datetime.now(timezone.utc))
 
     # ADD THIS LINE
-    is_active = db.Column(db.Boolean, default=True)  #  Add is_active
+    is_active = db.Column(db.Boolean, default=True)  # Add is_active
 
     # Relationships
-    interested_buyers = relationship('InterestedBuyers', backref='car', lazy=True)
+    interested_buyers = relationship('InterestedBuyers',
+                                       backref='car', lazy=True)
     wishlist_entries = relationship('Wishlist', backref='car', lazy=True)
     appointments = relationship('Appointment', lazy=True)
     images = relationship("Image", backref="car")  # Add the 'images' relationship
