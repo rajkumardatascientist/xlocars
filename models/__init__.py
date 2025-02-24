f6a70f0f243eef878394aa6122fab27a0fc2e395
rajkumar <rajkumardatascientist59@gmail.com>
2025-02-24 11:53:29 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 # models/__init__.py
-from .user import User, UserRole # Import User and UserRole
+from .user import User, UserRole  # Import User and UserRole
 from .car import Car
 from .appointment import Appointment
 from .payment import FeaturedPayments, BuyerPayments
 from .reported_ad import ReportedAds
 from .wishlist import Wishlist
 from .interested_buyers import InterestedBuyers
-from .image import Image # ADD THIS LINE
+from .image import Image  # ADD THIS LINE
