b615b4d0d0df7c0c316e421d0b9a718861560c2a
rajkumar <rajkumardatascientist59@gmail.com>
2025-03-07 12:13:28 +0530

    Update home_forms.py

--- forms/home_forms.py
+++ forms/home_forms.py
@@ -4,4 +4,8 @@ from wtforms.validators import DataRequired
 
 class MinimalForm(FlaskForm):
     make = StringField('Make', validators=[DataRequired()])
+    model = StringField('Model')
+    state = StringField('State')
+    district = StringField('District')
+    owner_type = StringField('Owner Type')
     submit = SubmitField('Apply Filters')
